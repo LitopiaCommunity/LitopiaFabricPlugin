1f4cb398d2380929aa9c3dd8b72edb7688248e74
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-11-14 11:11:41 +0100

    Mise en place du plugin de litopia pour fabric

--- src/main/java/fr/litopia/services/LitopiaServices.java
+++ src/main/java/fr/litopia/services/LitopiaServices.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package fr.litopia.services;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class LitopiaServices implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
