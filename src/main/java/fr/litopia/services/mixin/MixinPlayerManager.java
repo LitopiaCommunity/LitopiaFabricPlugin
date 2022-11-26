298e8280f7347ed853c05888d5311e5706f86edf
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-11-26 21:53:20 +0100

    Build : mise à jour en 1.19.2

--- src/main/java/fr/litopia/services/mixin/MixinPlayerManager.java
+++ src/main/java/fr/litopia/services/mixin/MixinPlayerManager.java
@@ -1,32 +1,33 @@
 package fr.litopia.services.mixin;
 
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.MessageType;
+import net.minecraft.network.message.MessageSourceProfile;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
+import java.util.function.Predicate;
 
 @Mixin(PlayerManager.class)
 public class MixinPlayerManager {
-    @Inject(method = "onPlayerConnect", at = @At("RETURN"))
-    private void playerConnected(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
-        System.out.println("Connexion du joueur : "+player.getEntityName());
+    @Inject(method = "onPlayerConnect", at = @At("HEAD"))
+    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+        System.out.println("Connexion de " + player.getName().toString());
     }
 
     @Inject(method = "remove", at = @At("HEAD"))
-    private void playerDisconnected(ServerPlayerEntity player, CallbackInfo ci) {
-        String playerStats = ((ServerStatHandlerAccessor) player.getStatHandler()).asString();
-        System.out.println(playerStats);
+    private void remove(ServerPlayerEntity player, CallbackInfo ci) {
+        System.out.println("Déconnexion du joueur : "+player.getEntityName());
     }
 
-    @Inject(method = "broadcastChatMessage", at = @At("HEAD"))
-    private void broadcastChatMessage(Text message, MessageType type, UUID senderUuid,CallbackInfo ci){
-        System.out.println("MSG : "+message.getString());
+    @Inject(method = "broadcast", at = @At("HEAD"))
+    private void broadcast(SignedMessage message, Predicate<ServerPlayerEntity> shouldSendFiltered, @Nullable ServerPlayerEntity sender, MessageSourceProfile sourceProfile, MessageType.Parameters params) {
+        System.out.println("MSG : "+message.getContent().getString());
     }
 }
